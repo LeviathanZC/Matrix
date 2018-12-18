657db90e449f59f7f41bffb00b82be8f3dd4a293
LeviathanZC <zerettecomp@gmail.com>
2018-12-18 19:14:10 +0300

    Добавлен метод создания матрицы.

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] creatMat (int a, int b){
+
+        return (int[][] mat = new int [a][b]);
+    }
+
+}
+
